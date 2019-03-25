24103dc0e61562522a04c11cc4b0ee95a17db6a6
Ariel <cantegone@yahoo.com>
2019-03-25 23:24:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+destructivelyAppendKitten("Ralph");
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+destructivelyPrependKitten("Bob");
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+destructivelyRemoveLastKitten();
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+destructivelyRemoveFirstKitten();
+
+//error
+function appendKitten(name) {
+  return [kittens,...name];
+}
+appendKitten("Broom");
+
+function prependKitten(name) {
+ return [name,...kittens];
+}
+prependKitten("Arnold");
+
+//error
+function removeLastKitten() {
+  return kittens.slice(kittens.length-1);
+}
+removeLastKitten();
+
+//error
+function removeFirstKitten() {
+  return kittens.slice(0);
+}
+removeFirstKitten();
